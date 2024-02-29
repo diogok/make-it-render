9d3dc879843259944ccf84dcc0f60ece3ee5724a
Diogo Silva <diogo@diogok.net>
2024-02-29 21:52:07 +0900

    fmt

--- src/windows/windows/windows.zig
+++ src/windows/windows/windows.zig
@@ -5,8 +5,8 @@ pub const Brush = *anyopaque;
 pub const Menu = *anyopaque;
 pub const WindowHandle = *anyopaque;
 
-pub const W =  @import("std").unicode.utf8ToUtf16LeWithNull;
-pub const W2 =  @import("std").unicode.utf8ToUtf16LeStringLiteral;
+pub const W = @import("std").unicode.utf8ToUtf16LeWithNull;
+pub const W2 = @import("std").unicode.utf8ToUtf16LeStringLiteral;
 
 pub extern "kernel32" fn GetModuleHandleExW(flags: u32, module_name: ?[*:0]const u16, module: ?*?Instance) callconv(.C) ?Instance;
 pub extern "kernel32" fn GetLastError() callconv(.C) u32;
@@ -25,7 +25,6 @@ pub const WindowClass = extern struct {
     class_name: ?[*:0]const u16,
     icon_small: ?Icon = null,
 };
-
 
 pub extern "user32" fn RegisterClassExW(window_class: ?*const WindowClass) callconv(.C) u16;
 
@@ -78,14 +77,14 @@ pub const ExtendedWindowStyle = enum(u32) {
 };
 
 pub const WindowStyle = enum(u32) {
-    Border   =    0x00800000,
-    Caption  =    0x00C00000,
-    Maximize   =  0x01000000,
+    Border = 0x00800000,
+    Caption = 0x00C00000,
+    Maximize = 0x01000000,
     MaximizeBox = 0x00010000,
-    Minimize    = 0x20000000,
+    Minimize = 0x20000000,
     MinimizeBox = 0x00020000,
-    SysMenu  =    0x00080000,
-    ThickFrame =  0x00040000,
+    SysMenu = 0x00080000,
+    ThickFrame = 0x00040000,
     OverlappedWindow = 0x00CF0000,
     Overlapped = 0x00000000,
 };
